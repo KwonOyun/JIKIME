787ea3506dcb97c677de2d6e1b6b4dfec7ab8c6f
KwonOyun <oyoon33@gmail.com>
2017-11-25 23:09:37 +0900

    init

---
+++ app/src/main/java/com/example/oyun/saveme/MainActivity.java
@@ -0,0 +1,229 @@
+package com.example.oyun.saveme;
+
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.ColorDrawable;
+import android.hardware.Camera;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    int flag;
+    private Camera camera;  //후레쉬 제어를 위해 Camera 객체 생성
+    boolean isFlashOn = true;  //후레쉬 점멸을 위한 boolean 값
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private SmsManager smsManager = SmsManager.getDefault();
+    private GPS gps;
+    SoundPool sp;   //경보음 재생을 위해 SoundPool API사용
+    int soundID ;
+    GoogleMap Map;
+
+//    ToggleButton emergencybutton;
+//    Button settingbuttion;
+//    Button newsbutton;
+
+    String phonenumber1;
+    String phonenumber2;
+    String phonenumber3;
+    String emergencysms;
+
+    String lat;
+    String lon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapcontainer);
+        mapFragment.getMapAsync(this);
+
+        sp = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);   //경보음 재생을 위해 SoundPool API사용
+        soundID = sp.load(this, R.raw.emergencysound, 1);
+
+//        emergencybutton = (ToggleButton) findViewById(R.id.emergencyButton);  //토클버튼으로 비상버튼 설정
+//        settingbuttion = (Button) findViewById(R.id.settingButton);   //설정버튼
+//        newsbutton = (Button) findViewById(R.id.newsButton);   //공지사항 버튼
+//
+//        settingbuttion.setOnClickListener(new View.OnClickListener() {   //설정 버튼을 눌렀을 경우
+//            @Override
+//            public void onClick(View view) {
+//                Intent settingintent = new Intent(MainActivity.this, Setting.class);  //설정화면으로 넘김
+//                MainActivity.this.startActivity(settingintent);
+//            }
+//        });
+//
+//        newsbutton.setOnClickListener(new View.OnClickListener() {  //공지사항 버튼을 눌렀을 경우
+//            @Override
+//            public void onClick(View view) {
+//                Intent newsintent = new Intent(MainActivity.this, News.class);  //공지사항화면으로 넘김
+//                MainActivity.this.startActivity(newsintent);
+//            }
+//        });
+
+//        emergencybutton.setOnClickListener(new View.OnClickListener() {    //경보 버튼을 눌렀을 때
+//            @Override
+//            public void onClick(View view) {
+//                if (emergencybutton.isChecked()) {
+//                    flag = 0;
+//                    blink();  //점멸 Turn On
+//                    SoundOn();  //소리 On
+//                    FindLocation();  //위도 경도 찾기
+//                    LoadData();   //설정되어 있는 번호 불러오기
+//                    SendMessage();  //설정되어 있는 번호로 문자 보내기
+//                    SendLocation();   //설정되어 있는 번호로 위치정보 보내기
+//                } else {
+//                    flag = 1;
+//                    SoundOff();  //소리 OFF
+//                    blink(); //점멸 Turn Off
+//                    LocationOff(); //GPS 종료
+//                }
+//            }
+//        });
+    }
+
+
+    private void blink() {  //점멸하는 메소드
+        Thread t = new Thread() {  //스레드 생성
+            public void run() {
+                try {
+                    while (flag != 1) { //점멸을 위해 while문 안에서 반복을 하여 후레쉬가 켜졌다 꺼졌다를 반복한다.
+                        if (isFlashOn) {
+                            turnOnFlash();
+                        } else {
+                            sleep(500);
+                            turnOffFlash();
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        t.start();
+    }
+
+    private void turnOnFlash() {  //Flash On
+
+        camera = Camera.open();
+        Camera.Parameters param = camera.getParameters();
+        param.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        camera.setParameters(param);
+        camera.startPreview();
+        isFlashOn = false;  //isFlashOn변수를 false로 설정하여 flash가 다시 꺼지게 함.
+    }
+
+    private void turnOffFlash() { //Flash off
+
+        Camera.Parameters param = camera.getParameters();
+        param.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        camera.setParameters(param);
+        camera.stopPreview();
+        camera.release();
+        isFlashOn = true;  //isFlashOn변수를 true로 설정하여 flash가 다시 켜지게 함.
+    }
+
+    public void SoundOn(){  //경보음을 울리는 메소드
+        sp.play(soundID, 1f, 1f, 0, -1, 1f); //경보음 시작
+    }
+
+    public void SoundOff(){  //경보음을 끄는 메소드
+        sp.stop(soundID); //경보음 중지
+    }
+
+    public void FindLocation(){  //위치정보를 얻어오는 메소드
+        gps = new GPS(MainActivity.this);   //위치 정보 얻어오기
+        if(gps.isGetLocation()){
+            double latitude = gps.getLatitude();   //위도 받아옴
+            double longitude = gps.getLongitude();   //경도 받아옴
+            lat = String.valueOf(latitude);
+            lon = String.valueOf(longitude);
+            Toast.makeText(getApplicationContext(), "위도:"+lat+"경도:"+lon, Toast.LENGTH_LONG).show();
+        }
+        else {
+            gps.showSettingsAlert();  //위치정보 허용이 되있지 않은 경우 설정창으로 이동하는 Alert창을 띄움
+        }
+    }
+
+    public void LocationOff(){  //GPS를 종료하는 메소드
+        gps.stopUsingGPS();
+    }
+
+    public void LoadData(){  //등록정보읽어오는 메소드
+        try{
+            FileInputStream fis = openFileInput("savemedatainformation.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+            phonenumber1 = reader.readLine();  //첫번째 번호
+            phonenumber2 = reader.readLine();  //두번째 번호
+            phonenumber3 = reader.readLine();  //세번째 번호
+            emergencysms = reader.readLine();  //메세지
+            Log.d("Test", phonenumber1);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void SendMessage(){   //위험 메세지를 보내주는 메소드
+        smsManager.sendTextMessage(phonenumber1, null,  emergencysms, null, null);   //첫번째 번호로 문자 메세지 보내기
+        smsManager.sendTextMessage(phonenumber2, null,  emergencysms, null, null);   //두번째 번호로 문자 메세지 보내기
+        smsManager.sendTextMessage(phonenumber3, null,  emergencysms, null, null);   //세번째 번호로 문자 메세지 보내기
+
+    }
+
+    public void SendLocation(){  //위치를 전송해주는 메소드
+        smsManager.sendTextMessage(phonenumber1, null,  "https://maps.google.com/?q="+lat+","+lon, null, null);  //위치 전송
+        smsManager.sendTextMessage(phonenumber2, null,  "https://maps.google.com/?q="+lat+","+lon, null, null);
+        smsManager.sendTextMessage(phonenumber3, null,  "https://maps.google.com/?q="+lat+","+lon, null, null);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Map = googleMap;
+
+        LatLng seoul = new LatLng(37.52487, 126.92723);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(seoul).title("서울이다 씨발");
+
+        Map.addMarker(markerOptions);
+        Map.moveCamera(CameraUpdateFactory.newLatLng(seoul));
+        Map.moveCamera(CameraUpdateFactory.zoomTo(15));
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/oyun/saveme/Setting.java
@@ -0,0 +1,170 @@
+package com.example.oyun.saveme;
+
+import android.content.Context;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+
+public class Setting extends AppCompatActivity {
+
+    static final int REQUEST_CONTACTS = 1000;
+    private ArrayAdapter adapter;
+    private Spinner spinner;
+
+    EditText p1;  //첫번째 번호
+    EditText p2;  //두번째 번호
+    EditText p3;  //세번째 번호
+    EditText smsmessage; //보낼 메시지 내용
+
+    Button settingbutton;  //설정 버튼
+    Button checkingbutton;  //등록 정보 확인 버튼
+
+    String receiveName;
+    String receiveNumber;
+    int flag;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        spinner = (Spinner) findViewById(R.id.residenceSpinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.residence, R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        p1 = (EditText) findViewById(R.id.Pnumber1);  //첫번째 번호
+        p2 = (EditText) findViewById(R.id.Pnumber2);  //두번째 번호
+        p3 = (EditText) findViewById(R.id.Pnumber3);  //세번째 번호
+        smsmessage = (EditText) findViewById(R.id.Smsmessage); //보낼 메시지 내용
+        settingbutton = (Button) findViewById(R.id.settingfinish);  //설정 버튼
+        checkingbutton = (Button) findViewById(R.id.settingcheck);  //등록 정보 확인 버튼
+
+        p1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flag =1;
+                LoadContract();
+            }
+        });
+        p2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flag =2;
+                LoadContract();
+            }
+        });
+        p3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flag =3;
+                LoadContract();
+            }
+        });
+
+        settingbutton.setOnClickListener(new View.OnClickListener() {  //설정버튼을 눌렀을 경우
+            @Override
+            public void onClick(View view) {
+                String p1data = p1.getText().toString();
+                String p2data = p2.getText().toString();
+                String p3data = p3.getText().toString();
+                String smsdata = smsmessage.getText().toString();
+                String residencedata = spinner.getSelectedItem().toString();
+                InsertData(p1data, p2data, p3data, smsdata, residencedata);      //데이터를 txt파일로 생성해주는 함수
+                Toast.makeText(getApplicationContext(), "설정이 완료되었습니다." , Toast.LENGTH_LONG).show();
+            }
+        });
+
+        checkingbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String contents = LoadData();   //입력된 정보를 Load하고 정보들을 리턴하는 메소드
+            }
+        });
+
+    }
+
+    public void InsertData(String first, String second, String third, String sms, String res){   //txt파일을 생성해주는 메소드
+        try{
+
+            FileOutputStream fos = openFileOutput("savemedatainformation.txt", Context.MODE_WORLD_READABLE);
+            PrintWriter writer = new PrintWriter(fos);
+            writer.println(first); writer.println(second); writer.println(third); writer.println(sms); writer.println(res);   //각각의 데이터들을 입력
+            writer.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public String LoadData(){  //등록정보확인
+        StringBuffer buffer = new StringBuffer();
+
+        try{
+            FileInputStream fis = openFileInput("savemedatainformation.txt");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+            String str = reader.readLine();
+
+            Log.d("Test", str);
+
+            while(str!=null) {
+                buffer.append(str + "\n");
+                str = reader.readLine();
+            }
+            Toast.makeText(getApplicationContext(), buffer.toString(), Toast.LENGTH_LONG).show();
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return buffer.toString();
+    }
+
+    public void LoadContract(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setData(ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+        startActivityForResult(intent, REQUEST_CONTACTS);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {  //전화번호부 정보를 받아주는 onActivityResult메소드
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != RESULT_OK){
+            return;
+        }
+        if(requestCode == REQUEST_CONTACTS){
+            Cursor cursor = getContentResolver().query(data.getData(), new String[]{
+                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER},null,null,null);
+
+            cursor.moveToFirst(); //이름 획득
+            receiveName = cursor.getString(0);
+            receiveNumber = cursor.getString(1);
+            cursor.close();
+            if(flag ==1){
+                p1.setText(receiveNumber.toString()+" ("+receiveName+")");
+            }
+            else if(flag==2){
+                p2.setText(receiveNumber.toString()+" ("+receiveName+")");
+            }
+            else if(flag==3){
+                p3.setText(receiveNumber.toString()+" ("+receiveName+")");
+            }
+        }
+    }
+}
